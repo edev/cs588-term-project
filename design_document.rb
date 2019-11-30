ffb91cf58473e1057d5b5780c39539533881eb94
Dylan Laufenberg <edev455@gmail.com>
2019-11-29 19:49:54 -0800

    Added View A / Question 1.

--- design_document.rb
+++ design_document.rb
@@ -1,3 +1,23 @@
 {
-    _id: '_design/termproject'
+    _id: '_design/termproject',
+    views: {
+        a: {
+            map: <<~'END'
+                function (doc) {
+                    var id_filter = new RegExp('^needs/');
+                    var year_matcher =  /\d+$/;
+                    if (doc._id.match(id_filter) != null) {
+                        var year = doc._id.match(year_matcher)[0];
+                        emit(
+                            [year, doc._id],
+                            {
+                                "goal": doc.goal,
+                                "gathered": doc.gathered
+                            }
+                        );
+                    }
+                }
+            END
+        },
+    }
 }
