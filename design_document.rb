9ce02557b8b6a7a6b93051ea017d8ca94125f352
Dylan Laufenberg <edev455@gmail.com>
2019-11-30 14:41:27 -0800

    Added Question 3, View B.

--- design_document.rb
+++ design_document.rb
@@ -19,5 +19,26 @@
                 }
             END
         },
+        b: {
+            map: <<~'END'
+                function (doc) {
+                    var id_filter = /^needs\//;
+                    if (doc._id.match(id_filter) != null) {
+                        doc.sign_ups.forEach(function (sign_up) {
+                            emit(
+                                [sign_up.email, sign_up.created_at],
+                                {
+                                    "need_title": doc.title,
+                                    "name": sign_up.name,
+                                    "phone": sign_up.phone,
+                                    "quantity": sign_up.quantity,
+                                    "comment": sign_up.comment
+                                }
+                            );
+                        });
+                    }
+                }
+            END
+        }
     }
 }
